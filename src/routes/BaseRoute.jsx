436813326a819e4bdc170f803762283a540da431
Darth Vve <osheviremajoroh@gmail.com>
2022-11-16 01:33:43 +0100

    Project Setup 1.0.1

---
+++ src/routes/BaseRoute.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ProtectedRoute from "./ProtectedRoute";
+import { Routes, Route } from "react-router-dom";
+import { SignUp, Login } from '../pages';
+
+
+const BaseRoute = () => {
+    return (
+        <Routes>
+            {/*Public routes*/}
+            {/* <Route path="/" element={< />} />
+            <Route path="/" element={< />} /> */}
+            <Route path="/register" element={<SignUp/>} />
+            <Route path="/login" element={<Login/>} />
+
+            
+            {/*Protected Routes*/}
+            <Route element={<ProtectedRoute/>}>
+                {/* <Route path="/userprofile/:id" element={< />} />
+                <Route path='/admin/dashboard' element={< />} />
+                <Route path="/dashboard/:id" element={< />} /> */}
+            </Route>
+
+            {/*Catch Errors*/}
+            {/* <Route path='*' element={<NotFound/>}/> */}
+        </Routes>
+    )
+};
+
+export default BaseRoute;
